e9d311c93325f6b0fef6574e1d580f2eff83eefe
Wamaitha <wamaithaweru19@gmail.com>
2022-07-30 23:06:02 +0300

    edit settings

--- Main/settings.py
+++ Main/settings.py
@@ -152,3 +152,10 @@ MEDIA_ROOT=os.path.join(BASE_DIR, "static")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='jaelweru5@gmail.com'
+EMAIL_HOST_PASSWORD=''
